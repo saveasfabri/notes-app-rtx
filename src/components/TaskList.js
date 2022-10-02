28121dda35dd51e8eb31edb742c829c4fba97a4f
Fabricio Hang <saveasfabri@gmail.com>
2022-10-02 07:41:36 -0300

    :tada: Add: Initial commit

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,6 @@
-import { useSelector } from "react-redux";
+/* import { useSelector } from "react-redux"; */
 
 const TaskList = () => {
-  const state = useSelector(state => state);
-  const tasks = useSelector(state => state.tasks);
-
-  console.log('state', state)
-  console.log('task', tasks)
 
   return (
       <h2>Task List</h2>
